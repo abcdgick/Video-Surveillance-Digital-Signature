6102b1831bedb1e34e6213703f11a7aa0b26eaa0
Jonathan Krisna <65582909+abcdgick@users.noreply.github.com>
2023-04-19 14:50:05 +0700

    Update camera_opencv.py

--- camera_opencv.py
+++ camera_opencv.py
@@ -32,4 +32,4 @@ class Camera(BaseCamera):
             _, img = camera.read()
 
             # encode as a jpeg image and return it
-            yield cv2.imencode('.jpg', img)[1].tobytes()
+            yield cv2.imencode('.jpg', img, encode_param)[1].tobytes()
